823f33ee605f902516a55a4726ed2f06179b30a4
Liam Carswell <liamcarswell@gmail.com>
2020-12-06 19:56:02 +0000

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
