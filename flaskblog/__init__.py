fd22238a220b322211e261964dd1334a22e20785
Liam Carswell <liamcarswell@gmail.com>
2020-12-07 09:58:01 +0000

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,31 +5,51 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
 
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
+login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail()
 
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    mail.init_app(app)
+from flaskblog.main.routes import main
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.errors.handlers import errors
 
-    from flaskblog.main.routes import main
-    from flaskblog.users.routes import users
-    from flaskblog.posts.routes import posts
-    from flaskblog.errors.handlers import errors
+app.register_blueprint(main)
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(errors)
 
-    app.register_blueprint(main)
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(errors)
 
-    return app
+#db = SQLAlchemy()
+#bcrypt = Bcrypt()
+#login_manager = LoginManager()
+#login_manager.login_view = 'users.login'
+#login_manager.login_message_category = 'info'
+#mail = Mail()
+
+
+# def create_app(config_class=Config):
+#app = Flask(__name__)
+# app.config.from_object(Config)
+# db.init_app(app)
+# bcrypt.init_app(app)
+# login_manager.init_app(app)
+# mail.init_app(app)
+
+#from flaskblog.main.routes import main
+#from flaskblog.users.routes import users
+#from flaskblog.posts.routes import posts
+#from flaskblog.errors.handlers import errors
+
+# app.register_blueprint(main)
+# app.register_blueprint(users)
+# app.register_blueprint(posts)
+# app.register_blueprint(errors)
+
+# return app
